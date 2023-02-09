6396099e9e259036b8f565784a8891ce37a7ae75
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-09 18:31:50 +0800

    Update ohp-ssh.sh

--- install/ohp-ssh.sh
+++ install/ohp-ssh.sh
@@ -1,5 +1,5 @@
 #wget https://github.com/${GitUser}/
-GitUser="Internetvps"
+GitUser="annelyah23"
 #Direct Proxy Squid For OpenVPN TCP
 RED='\e[1;31m'
 GREEN='\e[0;32m'
@@ -21,7 +21,7 @@ Port_OHP='8686';
 
 #Installing ohp Server
 cd 
-wget -O /usr/local/bin/ohps "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/ohps"
+wget -O /usr/local/bin/ohps "https://raw.githubusercontent.com/${GitUser}/homa/main/ohps"
 chmod +x /usr/local/bin/ohps
 
 #Buat Service Untuk OHP SSH
