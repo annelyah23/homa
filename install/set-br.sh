d09c7afba1994b5e3c92771d9d280569c920df8e
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-09 18:32:45 +0800

    Update set-br.sh

--- install/set-br.sh
+++ install/set-br.sh
@@ -1,5 +1,5 @@
 #wget https://github.com/${GitUser}/
-GitUser="Internetvps"
+GitUser="annelyah23"
 curl https://rclone.org/install.sh | bash
 printf "q\n" | rclone config
 wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/rclone.conf"
@@ -9,12 +9,12 @@ make install
 cd
 rm -rf wondershaper
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/system/backupBot.sh"
-wget -O backup "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/system/backup.sh"
-wget -O bckp "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/system/bckp.sh"
-wget -O restore "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/system/restore.sh"
-wget -O strt "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/system/strt.sh"
-wget -O limit-speed "https://raw.githubusercontent.com/${GitUser}/drgvpnscript/main/limit-speed.sh"
+wget -O autobackup "https://raw.githubusercontent.com/${GitUser}/homa/main/system/backupBot.sh"
+wget -O backup "https://raw.githubusercontent.com/${GitUser}/homa/main/system/backup.sh"
+wget -O bckp "https://raw.githubusercontent.com/${GitUser}/homa/main/system/bckp.sh"
+wget -O restore "https://raw.githubusercontent.com/${GitUser}/homa/main/system/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/${GitUser}/homa/main/system/strt.sh"
+wget -O limit-speed "https://raw.githubusercontent.com/${GitUser}/homa/main/limit-speed.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x bckp
