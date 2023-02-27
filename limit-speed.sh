b535ca00ad0f1cd0e2ca998f47cf8d8bbfc71064
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-27 11:22:59 +0800

    Update limit-speed.sh

--- limit-speed.sh
+++ limit-speed.sh
@@ -1,37 +1,32 @@
 #!/bin/bash
-GitUser="annelyah23"
-#IZIN SCRIPT
-MYIP=$(curl -sS ipv4.icanhazip.com)
-echo -e "\e[32mloading...\e[0m"
-clear
-# Valid Script
-VALIDITY () {
-    today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
-    else
-    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
-    echo -e "\e[31mPlease renew your ipvps first\e[0m"
-    exit 0
-fi
-}
-IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-VALIDITY
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-echo -e "\e[31mPlease buy script first\e[0m"
-exit 0
-fi
-clear
+# =========================================
+# Quick Setup | Script Setup Manager
+# Edition : Stable Edition V1.0
+# Auther  : JsPhantom
+# (C) Copyright 2023
+# =========================================
+# =========================================
+###########- COLOR CODE -##############
+colornow=$(cat /etc/JsPhantom/theme/color.conf)
+NC="\e[0m"
+RED="\033[0;31m" 
+COLOR1="$(cat /etc/JsPhantom/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+COLBG1="$(cat /etc/JsPhantom/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"   
+WH='\033[1;37m'                 
+###########- END COLOR CODE -##########
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+tyblue='\e[1;36m'
+purple='\e[0;35m'
+NC='\e[0m'
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[ON]${Font_color_suffix}"
 Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
 cek=$(cat /home/limit)
 NIC=$(ip -o $ANU -4 route show to default | awk '{print $5}');
 function start () {
+clear
 echo -e "Limit Speed All Service"
 read -p "Set maximum download rate (in Kbps): " down
 read -p "Set maximum upload rate (in Kbps): " up
@@ -44,15 +39,22 @@ wondershaper -a $NIC -d $down -u $up > /dev/null 2>&1
 systemctl enable --now wondershaper.service
 echo "start" > /home/limit
 echo "Done"
+sleep 1
+clear
+menu
 fi
 }
 function stop () {
+clear
 wondershaper -ca $NIC
 systemctl stop wondershaper.service
 echo "Stop Configuration"
 sleep 0.5
 echo > /home/limit
 echo "Done"
+sleep 1
+clear
+menu
 }
 if [[ "$cek" = "start" ]]; then
 sts="${Info}"
@@ -60,20 +62,29 @@ else
 sts="${Error}"
 fi
 clear
-echo -e " \e[0;32m==============================\e[0m"
-echo -e "     \e[1;36mLimit Bandwidth Speed\e[0m"
-echo -e " \e[0;32m==============================\e[0m"
-echo -e " Status $sts"
-echo -e "  1. Start Limit"
-echo -e "  2. Stop Limit"
-echo -e " Press CTRL+C to return"
-read -rp " Please Enter The Correct Number : " -e num
+echo -e "$COLOR1╔===========================================╗\033[0m"
+echo -e " $COLOR1${COLBG1}           ${WH}LIMIT BANDWITH SPEED            \033[0m"
+echo -e "$COLOR1╚===========================================╝\033[0m
+\e[1;31mLimit Bandwith Speed by${NC} \e[1;33mJsPhantom\033[0m
+\033[1;37mTelegram : https://t.me/JsPhantom / @JsPhantom\033[0m"
+echo ""
+echo -e "   Status : $sts"
+echo -e " ${WH}[${COLOR1}01${WH}]$NC${COLOR1}•${WH}Start Limit\033[0m"
+echo -e " ${WH}[${COLOR1}02${WH}]$NC${COLOR1}•${WH}Stop Limit\033[0m"
+echo -e " ${WH}[${COLOR1}03${WH}]$NC${COLOR1}•Back To Menu\033[0m"
+echo ""
+echo -e " \033[1;37mPress [ Ctrl+C ] • To-Exit-Script\033[0m"
+echo ""
+read -rp "Select menu : " -e num
 if [[ "$num" = "1" ]]; then
 start
 elif [[ "$num" = "2" ]]; then
 stop
+elif [[ "$num" = "3" ]]; then
+menu-other
 else
 clear
-echo " You Entered The Wrong Number"
-menu
+echo " Please Enter The Correct Number!"
+sleep 0.5
+limit
 fi
